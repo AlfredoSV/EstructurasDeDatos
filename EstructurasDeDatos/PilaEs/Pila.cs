5ac12099f492f51497e881e94cfa1cc3c84fc00c
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-02 09:30:51 -0600

    Se agrega método push a clase Pila

---
+++ EstructurasDeDatos/PilaEs/Pila.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstructurasDeDatos.PilaEs
+{
+    public class Pila
+    {
+
+        private Nodo _nodo;
+
+        public Pila()
+        {
+            _nodo = null;
+        }
+
+        public bool PilaVacia() => _nodo == null;
+
+        public void Push(int i)
+        {
+            if (PilaVacia())
+            {
+                _nodo = new Nodo(null, i);
+            }
+            else
+            {
+                var nodAux = _nodo;
+
+                while (true)
+                {
+                    if (nodAux.Siguiente == null)
+                    {
+                        nodAux.Siguiente = new Nodo(null, i);
+                        break;
+                    }
+
+                    nodAux = nodAux.Siguiente;
+                }
+
+
+            }
+        }
+
+
+
+
+    }
+}
--- EstructurasDeDatos/Program.cs
+++ EstructurasDeDatos/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using EstructurasDeDatos.ListaEnlazadaEs;
 using EstructurasDeDatos.ListaEnlazadaEsGenerica;
+using EstructurasDeDatos.PilaEs;
+
 
 namespace EstructurasDeDatos
 {
@@ -8,31 +10,46 @@ namespace EstructurasDeDatos
     {
         static void Main(string[] args)
         {
-            ListaEnlazadaGenerica<int> listaEnlazada = new ListaEnlazadaGenerica<int>();
-
+            /*ListaEnlazadaGenerica<int> listaEnlazada = new ListaEnlazadaGenerica<int>();
 
             listaEnlazada.AgregarNuevoElemento(1);
             listaEnlazada.AgregarNuevoElemento(2);
             listaEnlazada.AgregarNuevoElemento(3);
-            /*listaEnlazada.AgregarNuevoElemento(900);
+            listaEnlazada.AgregarNuevoElemento(900);
             listaEnlazada.AgregarNuevoElemento(6);
             listaEnlazada.AgregarNuevoElemento(0);
-            */
-            //listaEnlazada.RecorrerLista();
+            
+            listaEnlazada.RecorrerLista();
 
-            /*Console.WriteLine("Se elimina el 6");
+            Console.WriteLine("Se elimina el 6");
             listaEnlazada.EliminarElemento(6);
             Console.WriteLine("Se elimina el 0");
-            listaEnlazada.EliminarElemento(0);*/
-            //Console.WriteLine("Se elimina el 4");
-            //listaEnlazada.EliminarElemento(1);
-            /*Console.WriteLine("Se elimina el 900");
-            listaEnlazada.EliminarElemento(900);*/
+            listaEnlazada.EliminarElemento(0);
+            Console.WriteLine("Se elimina el 4");
+            listaEnlazada.EliminarElemento(1);
+            Console.WriteLine("Se elimina el 900");
+            listaEnlazada.EliminarElemento(900);
 
             listaEnlazada.RecorrerLista();
-            //Console.WriteLine($"El 10 existe en la lista:{listaEnlazada.BuscarElemento(10)}");
+            Console.WriteLine($"El 10 existe en la lista:{listaEnlazada.BuscarElemento(10)}");
 
-            Console.WriteLine($"La lista está vacia:{listaEnlazada.ListaVacia()}");
+            Console.WriteLine($"La lista está vacia:{listaEnlazada.ListaVacia()}");*/
+
+
+            Pila pila = new Pila();
+
+            Console.WriteLine(pila.PilaVacia());
+
+            pila.Push(3);
+            pila.Push(45);
+            pila.Push(24);
+
+            Console.WriteLine(pila.PilaVacia());
+
+
+
+
+
         }
     }
 
